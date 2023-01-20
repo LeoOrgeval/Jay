54bff8494fcd5c19485a0d1642ca196029a7be04
Leo Orgeval <leoorgeval.pro@gmail.com>
2023-01-20 17:48:50 +0100

    First day + Install Tailwind + Config Tailwind colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      orange: '#EF6D58',
+      dark: '#391400',
+      shadow: '#000000',
+    }
+  },
+  plugins: [],
+}
