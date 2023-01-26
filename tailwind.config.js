a5c0e81b70a93eea59cb4c45b10cd28a971fd75e
Leo Orgeval <leoorgeval.pro@gmail.com>
2023-01-26 17:43:44 +0100

    D5 Modif Homepage + Projects + Service

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       cream: '#FDF0E9',
       orange: '#EF6D58',
       dark_brown: '#391400',
+      light_brown: 'rgba(57,20,0,0.64)',
       shadow: 'rgba(0,0,0, .4)',
     }
   },
