985de50d5b37b3308c560c69e27ad99ddcb8f333
Leo Orgeval <leoorgeval.pro@gmail.com>
2023-01-30 09:50:55 +0100

    D6 All pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       calendly: '#006BFF',
       cream: '#FDF0E9',
       orange: '#EF6D58',
+      light_orange: 'rgba(239,109,88,0.69)',
       dark_brown: '#391400',
       light_brown: 'rgba(57,20,0,0.64)',
       shadow: 'rgba(0,0,0, .4)',
